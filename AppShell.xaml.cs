2012a4fdcd98ada33c62fa67d4767ce9a975afb2
Ed Giardina <ed@edgiardina.com>
2022-11-27 16:33:23 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBug_WrongBackButtonTitle;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        Routing.RegisterRoute("MainPage", typeof(MainPage));
+        Routing.RegisterRoute("SubPage", typeof(SubPage));
+    }
+}
---
+++ SubPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiBug_WrongBackButtonTitle;
+
+public partial class SubPage : ContentPage
+{
+	int count = 0;
+
+	public SubPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
